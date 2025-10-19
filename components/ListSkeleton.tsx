987a9aa858d460c2b6a0992741beec20d1a813f8
ahmadabdoul <44808233+ahmadabdoul@users.noreply.github.com>
2025-10-19 15:14:27 +0100

    Fix quotes in ListSkeleton component

--- components/ListSkeleton.tsx
+++ components/ListSkeleton.tsx
@@ -1,18 +1,22 @@
-// src/components/ListSkeleton.tsx
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { radii, spacing } from '../constants/tokens';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { radii, spacing } from "../constants/tokens";
 
 export default function ListSkeleton() {
   return (
     <View style={styles.container}>
-      <View style={[styles.row, { width: '90%' }]} />
-      <View style={[styles.row, { width: '95%' }]} />
+      <View style={[styles.row, { width: "90%" }]} />
+      <View style={[styles.row, { width: "95%" }]} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { paddingHorizontal: spacing.m, paddingTop: spacing.s },
-  row: { height: 80, borderRadius: radii.card, backgroundColor: '#EFEFEF', marginVertical: spacing.s }
+  row: {
+    height: 80,
+    borderRadius: radii.card,
+    backgroundColor: "#EFEFEF",
+    marginVertical: spacing.s,
+  },
 });
