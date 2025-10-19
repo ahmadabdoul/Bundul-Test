aa9dbc36f2f484cec1dec195b8e3df94dfef3f72
ahmadabdoul <44808233+ahmadabdoul@users.noreply.github.com>
2025-10-19 15:19:48 +0100

    Improve formatting of formatCurrency function
    Refactor formatCurrency function for better readability.

--- utils/format.ts
+++ utils/format.ts
@@ -1,5 +1,5 @@
-
-export function formatCurrency(amount: number, currency = 'USD') {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(amount);
-  }
-  
+export function formatCurrency(amount: number, currency = "USD") {
+  return new Intl.NumberFormat("en-US", { style: "currency", currency }).format(
+    amount
+  );
+}
